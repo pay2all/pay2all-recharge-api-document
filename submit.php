e1bc96aea40079b0d766e3eb21f14d973723cf84
sanjay live <i@sanjay.live>
2017-10-21 21:04:10 +0530

    New udpates
    New updates

--- submit.php
+++ submit.php
@@ -5,55 +5,43 @@
 $provider_id = $_POST['provider_id']; 
 $number = $_POST['number'];
 $amount = $_POST['amount'];
+$client_id = "09919190"; //(your system unique id. that you can check in pay2all);
 //end of data collection from form
 
 
 //check whether user enter some data or not
-if(empty($provider_id)){
-echo"select operator";
-exit;
-}
-if(empty($number)){
-echo"enter mobile number";
-exit;
-}
-if(empty($amount)){
-echo"enter amount";
-exit;
-}
-
-
-
-        $parameters = array(
-            'number' => $number,
-            'provider_id' => $provider_id,
-            'amount' => $amount,
-            'vendor_id' => 1,
-        );
-
-        $key = "Personal Access Tokens";
-
-        $header = ["Accept:application/json", "Authorization:Bearer ".$key];
-
-        $method = 'POST';
-
-        $url = "https://www.pay2all.in/api/v1/transaction";
-
-
+        if(empty($provider_id)){
+        echo"select operator";
+        exit;
+        }
+        if(empty($number)){
+        echo"enter mobile number";
+        exit;
+        }
+        if(empty($amount)){
+        echo"enter amount";
+        exit;
+        }
+        $api_token = "api_token"; // api_token token will in (https://www.pay2all.in/developers/recharge-api-doc) 
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLINFO_HEADER_OUT, 1);
-        curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($parameters));
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $url = "https://www.pay2all.in/web-api/paynow?api_token=$api_token&number=$number&provider_id=$provider_id&amount=$amount&client_id=$client_id";
         curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $response = curl_exec($ch);
-        echo $response;  //[JSON RESPONSE]
+        curl_close($ch);
+
+
+$maindata = json_decode($response);
+
+$transactionid = $maindata['operator_ref'];
+$status = $maindata['status']; 
+$payid = $maindata['payid']; //pay2all order id
+
+//display the result to customer
+echo"Transaction ID: $transactionid (This is Operator reference ID)";
+echo"<br/>";
+echo"Recharge Status: $status";
+echo"<br/>";
 
 
 ?>

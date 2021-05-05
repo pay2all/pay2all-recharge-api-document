0ba3c50abd36252411c26a64c4a7de00462cfe3a
Pay2All <info@pay2all.in>
2021-05-05 13:19:29 +0530

    Update submit.php

--- submit.php
+++ submit.php
@@ -22,14 +22,47 @@ $client_id = "09919190"; //(your system unique id. that you can check in pay2all
         echo"enter amount";
         exit;
         }
-        $api_token = "api_token"; // api_token token will in (https://www.pay2all.in/developers/recharge-api-doc) 
+
+
+        $parameters = array(
+                'number' => $mobile_number,
+                'provider_id' => $provider_id // Provider id check in pay2all
+                'amount' => $amount, // Recharge or payment Amount
+                'client_id' => $client_id // your system unique id
+                'optional1' => '',
+                'optional2' => '',
+                'optional3' => '',
+                'optional4' => ''
+        );
+
+        $key = ""; //you have to add personal access token 
+
+    
+
+        $header = ["Accept:application/json", "Authorization:Bearer ".$key];
+
+        $method = 'POST';
+
+        $url = 'https://pay2all.in/api/v1/transaction';
+
+
         $ch = curl_init();
-        $url = "https://www.pay2all.in/web-api/paynow?api_token=$api_token&number=$number&provider_id=$provider_id&amount=$amount&client_id=$client_id";
-        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLINFO_HEADER_OUT, 1);
+        curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($parameters));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL, $url);
         $response = curl_exec($ch);
-        curl_close($ch);
-        echo $response;
+        echo $response;  //[JSON RESPONSE]
+
+
+     
 
 
 ?>
